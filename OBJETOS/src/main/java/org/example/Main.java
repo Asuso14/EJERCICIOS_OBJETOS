f9cd8d7f04c01740879c920b7149e76c1bb09957
NASHELAPTOP\Usuario <nachogamallolafon123@gmail.com>
2025-01-20 21:08:20 +0100

    fin clase persona

--- OBJETOS/src/main/java/org/example/Main.java
+++ OBJETOS/src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona persona1 = new Persona("Nacho","Gamallo","48670809A",90,21,"Alicante","Soporte Técnico");
+        Persona persona2 = new Persona();
+        persona1.printInfo();
+        System.out.println(persona1.Concat());
+        System.out.println("Antes: " + persona1.getName());
+        persona1.setName("Nashe");
+        System.out.println("Despues: " + persona1.getName());
     }
 }
---
+++ OBJETOS/src/main/java/org/example/Persona.java
@@ -0,0 +1,74 @@
+package org.example;
+
+public class Persona {
+
+    private String name;
+    private String surname;
+    private String DNI;
+    private int weight;
+    private int age;
+    private String ProvidenceCity;
+    private String Profession;
+
+    public Persona(){}
+
+    public Persona(String name, String surname, String DNI, int weight, int age, String providenceCity, String profession){
+        this.name = name;
+        this.surname = surname;
+        this.DNI = DNI;
+        this.weight = weight;
+        this.age = age;
+        this.ProvidenceCity = providenceCity;
+        this.Profession = profession;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
+    public String getDNI(){
+        return this.DNI;
+    }
+    public int getWeight(){
+        return this.weight;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getProvidenceCity(){
+        return this.ProvidenceCity;
+    }
+    public String getProfession(){
+        return this.Profession;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public void setDNI(String DNI){
+        this.DNI = DNI;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setProvidenceCity(String providenceCity){
+        this.ProvidenceCity = providenceCity;
+    }
+    public void setProfession(String profession){
+        this.Profession = profession;
+    }
+    public String Concat(){
+        return this.name + " " + this.surname;
+    }
+
+    public void printInfo(){
+        System.out.println(this.name +" "+ this.surname +" "+ this.DNI +" "+ this.age +" "+ this.ProvidenceCity +" "+ this.Profession);
+    }
+}
