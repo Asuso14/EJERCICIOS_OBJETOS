85c82887197cb6737a7f49010c8c16d02eab4713
NASHELAPTOP\Usuario <nachogamallolafon123@gmail.com>
2025-01-30 15:38:48 +0100

    SuperMarket casi terminado.

--- OBJETOS/src/main/java/org/example/SupermarketList.java
+++ OBJETOS/src/main/java/org/example/SupermarketList.java
@@ -14,12 +14,51 @@ public class SupermarketList{
         marketList.add(value);
     }
 
-    public ArrayList<String> getMarketList() {
-        return marketList;
+    public void getMarketList() {
+        if (checkList()){
+            System.out.println("La lista esta vacia, no puedo mostrar nada");
+        }else {
+            StringBuilder temp;
+            temp = new StringBuilder();
+            for (String list : marketList) temp.append(list).append(",");
+            System.out.println(temp);
+        }
     }
 
-    public void setMarketList(ArrayList<String> marketList) {
-        this.marketList = marketList;
+    private boolean searchProduct(String product){
+        return marketList.contains(product);
+    }
+
+    public void checkProduct(String product){
+        if (searchProduct(product)){
+            System.out.println("El producto esta dado de alta.");
+        }else {
+            System.out.println("El producto no esta dado de alta.");
+        }
+    }
+
+    public void getPosMarketList(int pos){
+        System.out.println(this.marketList.get(pos));
+    }
+
+    public void changeValueOfList(int pos, String value){
+        if (searchProduct(value)){
+            System.out.println("El producto ya esta dado de alta.");
+        }else {
+            this.marketList.add(pos, value);
+        }
+    }
+
+    public int numOfProducts(){
+        return this.marketList.size();
+    }
+
+    public void setMarketList(String product) {
+        if (searchProduct(product)){
+            System.out.println("El producto ya esta dado de alta.");
+        }else {
+            this.marketList.add(product);
+        }
     }
 
     public void emptyList(){
@@ -35,10 +74,47 @@ public class SupermarketList{
         return marketList.isEmpty();
     }
 
-    public static void main (String[] args){
+    public void numToAdd(int num){
+        if (num > 0){
+            System.out.println("Introduce producto: ");
+            setMarketList(entry.next());
+            numToAdd(--num);
+        }
+    }
+
+    public void listSimiles(SupermarketList list){
+        if (checkSimile(list)){
+            System.out.println("Las dos listas son iguales.");
+        }else {
+            System.out.println("Las dos listas no son iguales.");
+        }
+    }
+
+    private boolean checkSimile(SupermarketList list){
+        return this.marketList.equals(list);
+    }
+
+    public void cleanList(){
+        this.marketList.clear();
+    }
+
+    public void cloneList(){
+       Object temp = this.marketList.clone();
+       return temp;
+    }
+
+    public static void main (String[] args) throws CloneNotSupportedException {
 
         SupermarketList list1 = new SupermarketList();
         list1.emptyList();
         list1.emptyList();
+        SupermarketList list2 = new SupermarketList();
+        list2.numToAdd(2);
+        list2.getMarketList();
+        list1.getMarketList();
+        System.out.println(list2.numOfProducts());
+        System.out.println(list1.numOfProducts());
+        SupermarketList list3 = new SupermarketList();
+
     }
 }
