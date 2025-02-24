6a9541683f3554fa9ebe6f6e89f37e3e3e6979d1
Nacho <nachogamallolafon123@gmail.com>
2025-02-24 12:52:02 +0100

    Persona, avanzado

---
+++ OBJETOS/src/main/java/org/example/Festival/MinAgeException.java
@@ -0,0 +1,11 @@
+package org.example.Festival;
+
+public class MinAgeException extends RuntimeException{
+
+    public MinAgeException(){
+
+        System.out.println("Edad no válida");
+        
+    }
+
+}
--- OBJETOS/src/main/java/org/example/Festival/Persona.java
+++ OBJETOS/src/main/java/org/example/Festival/Persona.java
@@ -1,13 +1,17 @@
 package org.example.Festival;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Persona {
 
-    String nombre;
-    int edad;
+    private static final int MIN_AGE = 18;
+    private String nombre;
+    private int edad;
 
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
-        this.edad = edad;
+        setAge(edad);
     }
 
     public void mostrarInfo() {
@@ -17,5 +21,14 @@ public class Persona {
 
     public void accederEvento(){
         System.out.println("Accediendo al evento...");
+    }
+
+    public void setAge(int age) throws MinAgeException{
+
+        this.edad = age;
+        if (edad < MIN_AGE){
+            throw new MinAgeException();
+
+        }
     }
 }
